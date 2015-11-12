d81a80704caa2c10e6026d5790a9fe499762a343
Cory Forsyth <cory.forsyth@gmail.com>
2015-11-12 15:44:06 -0500

    make ready to be used as addon

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-mobiledoc-html-renderer'
+  name: 'ember-mobiledoc-html-renderer',
+  included: function() {
+    var libRoot = require.resolve('mobiledoc-html-renderer/lib');
+    var libPath = path.dirname(libRoot);
+
+    this.treePaths.addon = libPath;
+  }
 };
